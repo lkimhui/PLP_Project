fa86bf8c316ef3eff88471f0b0fa0252f0f3ae21
shashi.vish <shashi.vish@yahoo.in>
2023-10-17 01:16:56 +0800

    refactoring code repo

---
+++ scripts/prediction.py
@@ -0,0 +1,10 @@
+from model_helper import call_t5_base_fine_tune_512
+
+
+def generate_cover_letter(model_name, input):
+    cover_letter = ""
+
+    if model_name == "t5-base-fine-tune-512":
+        cover_letter = call_t5_base_fine_tune_512(model_name,input)
+
+    return cover_letter
